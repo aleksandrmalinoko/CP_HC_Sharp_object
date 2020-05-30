8f7de1818b2932bd052fda71ccd3f7d6078b199a
Teosof <liskunov.roma@yandex.ru>
2020-05-30 12:17:33 +0300

    Add chat

---
+++ app/chat/routes.py
@@ -0,0 +1,38 @@
+from flask_login import login_required
+from flask import session, redirect, url_for, render_template, request
+
+from app.chat import blueprint
+from app.chat.forms import LoginForm
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+    return render_template(template + '.html')
+
+
+@blueprint.route('/chat/base', methods=['GET', 'POST'])
+@login_required
+def index():
+    """Login form to enter a room."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['room'] = form.room.data
+        return redirect(url_for('.chat'))
+    elif request.method == 'GET':
+        form.name.data = session.get('name', '')
+        form.room.data = session.get('room', '')
+    return render_template('base.html', form=form)
+
+
+@blueprint.route('/chat/chat')
+@login_required
+def chat():
+    """Chat room. The user's name and room must be stored in
+    the session."""
+    name = session.get('name', '')
+    room = session.get('room', '')
+    if name == '' or room == '':
+        return redirect(url_for('.index'))
+    return render_template('chat.html', name=name, room=room)
